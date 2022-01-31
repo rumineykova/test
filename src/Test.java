0fab3fbb9c7394fa7dfdb56e7ab14d6d88f79919
Rumyana Neykova <rumi.neykova@gmail.com>
2022-01-31 15:39:21 +0000

    Changes to my branch

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("This is gits version");
+		System.out.print("This is my branch only");
 
 	}
 
