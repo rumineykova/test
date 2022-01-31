70f18cc0541430507d70d479720259f9a5b66ecd
Rumyana Neykova <rumi.neykova@gmail.com>
2022-01-31 15:18:20 +0000

    Added Hello world

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("This is my version");
+		System.out.print("hello world!");
 
 	}
 
