e073774347b8bd875dad8214e86112314e5d67a1
rumineykova <rumi.neykova@gmail.com>
2022-01-31 15:32:01 +0000

    Conflict in git

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Have a lovely day!");
+		System.out.print("This is gits version");
 
 	}
 
